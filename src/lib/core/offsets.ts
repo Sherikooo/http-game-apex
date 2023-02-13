548554114913d7449e5106addc659d7d00cc534e
Sherikooo <125220304+Sherikooo@users.noreply.github.com>
2023-02-13 17:27:39 +0100

    Update offsets.ts

--- src/lib/core/offsets.ts
+++ src/lib/core/offsets.ts
@@ -1,21 +1,21 @@
 export const offsets = {
   button: {
-    inSpeed: 0x75c6288,               // [Buttons]                        -> in_speed
-    inAttack: 0x75c62e0               // [Buttons]                        -> in_attack
+    inSpeed: 0x0766a900,               // [Buttons]                        -> in_speed
+    inAttack: 0x0766a958               // [Buttons]                        -> in_attack
   },
   core: {
-    levelName: BigInt(0x13a17b8),     // [Miscellaneous]                  -> LevelName
-    clEntityList: BigInt(0x1a98758),  // [Miscellaneous]                  -> cl_entitylist
+    levelName: BigInt(0x144df40),     // [Miscellaneous]                  -> LevelName
+    clEntityList: BigInt(0x1b2e708),  // [Miscellaneous]                  -> cl_entitylist
     localPlayer: BigInt(0x1e49268)    // [Miscellaneous]                  -> LocalPlayer
   },
   entity: {
-    localOrigin: 0x158,               // [DataMap.CBaseViewModel]         -> m_localOrigin
-    iSignifierName: 0x580,            // [RecvTable.DT_BaseEntity]        -> m_iSignifierName
+    localOrigin: 0x0054,               // [DataMap.CBaseViewModel]         -> m_localOrigin
+    iSignifierName: 0x0580,            // [RecvTable.DT_BaseEntity]        -> m_iSignifierName
     lastVisibleTime: 0x1670           // [Miscellaneous]                  -> CPlayer!lastVisibleTime
   },
   item: {
-    highlightFunctionBits: 0x2c0,     // [RecvTable.DT_HighlightSettings] -> m_highlightFunctionBits
-    customScriptInt: 0x1628           // [RecvTable.DT_PropSurvival]      -> m_customScriptInt
+    highlightFunctionBits: 0x02c0,     // [RecvTable.DT_HighlightSettings] -> m_highlightFunctionBits
+    customScriptInt: 0x1648         // [RecvTable.DT_PropSurvival]      -> m_customScriptInt
   },
   player: {
     glowEnable: 0x3c0 + 0x8,                // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x8
@@ -23,8 +23,8 @@ export const offsets = {
     iTeamNum: 0x44c,                  // [RecvTable.DT_BaseEntity]        -> m_iTeamNum
     iName: 0x589,                     // [RecvTable.DT_BaseEntity]        -> m_iName
     lifeState: 0x798,                 // [RecvTable.DT_Player]            -> m_lifeState
-    vecPunchWeaponAngle: 0x2440,      // [DataMap.C_Player]               -> m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
-    viewAngle: 0x253c - 0x14,                // [DataMap.C_Player]        -> m_ammoPoolCapacity - 0x14
-    bleedoutState: 0x26d0             // [RecvTable.DT_Player]            -> m_bleedoutState
+    vecPunchWeaponAngle: 0x0040,      // [DataMap.C_Player]               -> m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
+    viewAngle: 0x25a4 - 0x14,                // [DataMap.C_Player]        -> m_ammoPoolCapacity - 0x14
+    bleedoutState: 0x2738           // [RecvTable.DT_Player]            -> m_bleedoutState
   }
 };
